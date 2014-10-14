768c1115f14721ad91469489548d1b170a4ea071
ferron <ferronrsmith@gmail.com>
2014-10-14 12:39:16 -0400

    change tabs to space

--- bin/tasks/remove.sh
+++ bin/tasks/remove.sh
@@ -31,39 +31,39 @@ function btask.remove.run () {
   local project="$1"
   if [ -n "$project" ]; then
     (
-      cd "$(b.get bang.working_dir)"		  
-		local path="$(b.get bang.working_dir)/$project"
-	  _project_folder_exists?
-	  if _is_bangsh_project?; then
-		echo "removing project $project"
-		rm -rf "$path"
-	  fi 
+      cd "$(b.get bang.working_dir)"
+        local path="$(b.get bang.working_dir)/$project"
+      _project_folder_exists?
+      if _is_bangsh_project?; then
+        echo "removing project $project"
+        rm -rf "$path"
+      fi
     )
   fi
-}	
+}
 
 function _project_folder_exists? () {
-	if b.path.dir? $path; then
-		return 0;
-	else 
-		return -1;
-	fi
+    if b.path.dir? $path; then
+        return 0;
+    else
+        return -1;
+    fi
 }
 
 function _is_bangsh_project? () {
-	declare -a array=("modules" "tasks")
-	for f in "${array[@]}"
-	do
-		if b.path.dir? "$path/$f"; then
-			echo "valid"
-		else
-			return -1;
-		fi
-	done
-	
-	if b.path.file? "$path/$project"; then
-		echo "valid"
-	else
-		return -1
-	fi
+    declare -a array=("modules" "tasks")
+    for f in "${array[@]}"
+    do
+        if b.path.dir? "$path/$f"; then
+            echo "valid"
+        else
+            return -1;
+        fi
+    done
+
+    if b.path.file? "$path/$project"; then
+        echo "valid"
+    else
+        return -1
+    fi
 }
