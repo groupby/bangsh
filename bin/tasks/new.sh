89bccb59979ecac29964a945aef4c76181129a13
ferron <ferronrsmith@gmail.com>
2014-10-14 12:29:58 -0400

    added remove

--- bin/tasks/new.sh
+++ bin/tasks/new.sh
@@ -22,7 +22,18 @@
 #     #     |-- modules/.gitkeep
 #     #     |-- tasks/.gitkeep
 #     #     |-- task_new
+
+b.module.require opt
+
 function btask.new.run () {
+	b.try.do _run "$@"
+	b.catch ArgumentError b.opt.show_usage
+	b.try.end
+}
+
+function _run () {
+  _load_options
+  b.opt.init "${@:2}" # skip first argument as that denotes the task being processed
   local project="$1"
   if [ -n "$project" ]; then
     (
@@ -31,10 +42,19 @@ function btask.new.run () {
 
       _create_module_path
       _create_tasks_path
-      _create_main_file
+      if b.opt.has_flag? --self; then
+		_create_self_main_file
+      else
+      	_create_main_file
+      fi
       _copy_bangsh_to_project
     )
   fi
+}	
+
+function _load_options () {
+  # Help (--help and -h added as flags)
+  b.opt.add_flag --self "Self containing and not linked"
 }
 
 function _create_module_path () {
@@ -51,11 +71,20 @@ function _copy_bangsh_to_project () {
   cp -r "$_BANG_PATH" "$project/bangsh"
 }
 
-function _create_main_file () {
+function _create_self_main_file () {
   local project_name="$(basename "$project")"
   exec >> "$project/$project_name"
 
   echo '#!/usr/bin/env bash'
   echo "source 'bangsh/bang.sh'"
+  chmod +x "$project/$project_name"
+}
+
+function _create_main_file () {
+  local project_name="$(basename "$project")"
+  exec >> "$project/$project_name"
+
+  echo '#!/usr/bin/env bash'
+  echo "source '$_BANG_PATH/bang.sh'"
   chmod +x "$project/$project_name"
 }
